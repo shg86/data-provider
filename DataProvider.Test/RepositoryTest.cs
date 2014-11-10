64b125b03ec6765093b72da286d99f2c38af19f3
SHG86 <stfn.hijmering@gmail.com>
2014-11-10 12:16:45 +0100

    Removed Diagnostics class library project.
    Moved Diagnostics project to a different repository (diagnostics).
    Replaced the project with a DLL of the diagnostics project. Also added
    an unit testing project for the dataprovider to the solution.

---
+++ DataProvider.Test/RepositoryTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataProvider.Test
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        //TODO: add test methods.
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
--- DataProvider/Repository.cs
+++ DataProvider/Repository.cs
@@ -1,4 +1,4 @@
-﻿using CustomDiagnostics;
+﻿using Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Data;
